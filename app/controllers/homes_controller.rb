5fe0f88af42bfd1aa9b3d5dbe74100cb2a4c2e69
terukouki07 <azumacha08@gmail.com>
2024-01-04 00:17:10 +0000

    [finish]devise

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,7 @@
+class HomesController < ApplicationController
+  def top
+  end
+
+  def mypage
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+  }
+  root to: 'homes#top'
+  get 'homes/mypage'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
