2900c298a9814756d4a635881eabb7d200355212
terukouki07 <azumacha08@gmail.com>
2024-01-06 03:20:54 +0000

    finish[user]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,17 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @post_images = @user.post_images 
+    @post_images = @user.post_images
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
-    @user.update
-    redirect_to _path
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   private
