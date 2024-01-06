0ef82040e748078d80a9de59a2b46959d4b72f8f
terukouki07 <azumacha08@gmail.com>
2024-01-06 05:36:50 +0000

    commentコントローラ作成

---
+++ app/controllers/post_comments_controller.rb
@@ -0,0 +1,15 @@
+class PostCommentsController < ApplicationController
+  def create
+   post_image = PostImage.find(params[:post_image_id])
+   comment = PostComment.new(post_comment_params)
+   comment.user_id = current_user.id
+   comment.post_image_id = post_image.id
+   comment.save
+   redirect_to post_image_path(post_image)
+  end
+  
+  private
+  def post_comment_params
+    params.require(:post_comment).permit(:comment, :user_id, :post_image_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   get 'chat/:id' => 'chats#show', as: 'chat'
   resources :chats, only: [:create]
   get 'homes/about' => 'homes#about', as: 'about'
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create]
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
