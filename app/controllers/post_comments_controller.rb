732929f1cb4a2e6cc66e6b5e4c35ac430ed5e726
terukouki07 <azumacha08@gmail.com>
2024-01-06 07:20:35 +0000

    [finish]comment

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,11 +1,16 @@
 class PostCommentsController < ApplicationController
   def create
-   post_image = PostImage.find(params[:post_image_id])
-   comment = PostComment.new(post_comment_params)
-   comment.user_id = current_user.id
-   comment.post_image_id = post_image.id
-   comment.save
-   redirect_to post_image_path(post_image)
+    post_image = PostImage.find(params[:post_image_id])
+    comment = PostComment.new(post_comment_params)
+    comment.user_id = current_user.id
+    comment.post_image_id = post_image.id
+    comment.save
+redirect_to post_image_path(post_image)
+  end
+  
+  def destroy
+    PostComment.find(params[:id]).destroy
+    redirect_to post_image_path(params[:post_image_id])
   end
   
   private
